dbae84b9c2606aed1ecfc8bdce376cb8633f269d
Ojas <149180655+ojasarora77@users.noreply.github.com>
2024-04-14 09:22:28 +0100

    sc

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    XRPL: {
+      url: process.env.RPC, 
+      accounts: process.env.PRIVKEY
+    }
+  }
 };
